da65419d61bb3343013ef46631e07ef60ad384c2
Yusuf <holla2specialyusuf@gmailcom>
2023-12-02 04:57:47 +0100

    working compose page

--- app.js
+++ app.js
@@ -29,20 +29,20 @@ app.get('/contact', (req, res) => {
   res.render('contact', {contactContent: contactContent})
 
 })
-app.get('/post', (req, res) => {
-  res.render('post')
+app.get('/compose', (req, res) => {
+  res.render('compose')
 
 })
 
 
-app.post('/', (req, res) => {
+app.post('/compose', (req, res) => {
+ console.log(req.body.postTitle)
+
+})
+// app.post('/', (req, res) => {
  
 
-})
-app.post('/', (req, res) => {
- 
-
-})
+// })
 
 
 
